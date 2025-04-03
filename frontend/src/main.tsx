65e1afaf7cc407c6ce7696f17dcc91d57edf68ed
Ramses <ramses.hdz30@gmail.com>
2025-04-02 21:14:58 -0600

    Update main.tsx file to add the new user route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import UserProfileRoute from './components/UserProfileRoute.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/uid/:id" element={<UserProfileRoute />} />
     </Routes>
   </BrowserRouter>
 );
